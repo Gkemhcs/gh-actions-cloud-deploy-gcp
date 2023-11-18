0c6c4c9689da5d0803444dfd541b35394009703c
Gkemhcs <gudikotieswarmani@gmail.com>
2023-11-18 05:40:13 +0000

    added

--- app/server.py
+++ app/server.py
@@ -6,6 +6,6 @@ def home():
        return render_template("home.html",title="GITHUB-ACTIONS TO CLOUD-DEPLOY DEMO ",content="THIS PROJECTS SHOWS THE INTERGRATION OF GITHUB ACTIONS WITH CLOUD DEPLOY FOR CI/CD  ") 
 @app.route("/health")
 def health_check():
-        return " i am healthy"
+        return " healthy"
 if(__name__=="__main__"):
      app.run(port=8080,host="0.0.0.0")
