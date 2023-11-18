f0b77f49a17a867d90bda3b1dcef20f4ba90e78c
Gkemhcs <gudikotieswarmani@gmail.com>
2023-11-18 09:49:41 +0000

    added

--- app/server.py
+++ app/server.py
@@ -4,9 +4,9 @@ app=Flask("__main__")
 environ=os.environ.get("ENVIRONMENT")
 @app.route("/")
 def home():
-       return render_template("home.html",title=f"GITHUB-ACTIONS TO CLOUD-DEPLOY DEMO {environ} ",content="THIS PROJECTS SHOWS THE INTERGRATION OF GITHUB ACTIONS WITH CLOUD DEPLOY FOR CI/CD  ") 
+       return render_template("home.html",title=f"GITHUB-ACTIONS TO CLOUD-DEPLOY DEMO running in {environ.upper()} environment ",content="THIS PROJECTS SHOWS THE INTERGRATION OF GITHUB ACTIONS WITH CLOUD DEPLOY FOR CI/CD  ") 
 @app.route("/health")
 def health_check():
-        return "i am healthy"
+        return "healthy"
 if(__name__=="__main__"):
      app.run(port=8080,host="0.0.0.0")
